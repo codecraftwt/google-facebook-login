ab581afb212d6102fbabbd06e6692f6e96b715bf
codecraftwt <codecraftwt@gmail.com>
2024-11-11 18:33:59 +0530

    updated latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { SocialAuthService, GoogleLoginProvider, SocialUser, FacebookLoginProvider, } from '@abacritt/angularx-social-login';
-import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -11,41 +8,10 @@ import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 export class AppComponent implements OnInit {
 
-  loginForm!: FormGroup;
-  socialUser!: SocialUser;
-  isLoggedin?: boolean;
-  faFacebook = faFacebook;
-  faGoogle = faGoogle;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private socialAuthService: SocialAuthService
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
 
-    // this.socialAuthService.authState.subscribe((user) => {
-    //   this.socialUser = user;
-    //   this.isLoggedin = user != null;
-    //   console.log(this.socialUser);
-    // });
   }
 
-  onLogin(data: any) { }
-
-  signWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
-  }
-
-  signInWithFB(): void {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
-  }
-
-  logOut(): void {
-    this.socialAuthService.signOut();
-  }
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faProductHunt } from '@fortawesome/free-brands-svg-icons';
+import { faJediOrder } from '@fortawesome/free-brands-svg-icons';
+import { faSquarePollVertical } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,5 +13,10 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 })
 export class SidebarComponent {
 
-  FaDashboard = faGoogle
+  FaDashboard = faGoogle;
+  FaProductHunt = faProductHunt;
+  FaJediOrder = faJediOrder;
+  FaSquarePollVertical = faSquarePollVertical;
+  FaMagnifyingGlassChart = faMagnifyingGlassChart;
+  FaRightFromBracket = faRightFromBracket;
 }
